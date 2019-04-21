98e47fcf967d70001a3de0ae5103747012a681fa
pelegs <pelegs@gmail.com>
2019-04-21 16:23:57 +0200

    WIP

--- nonogram.py
+++ nonogram.py
@@ -5,8 +5,11 @@ import matplotlib
 import numpy as np
 
 
-def analyze_row(img, row_num):
-    row = img[row_num]
+def analyze(img, index, type='rows'):
+    if type == 'rows':
+        row = img[index,:]
+    else:
+        row = img[:,index]
     groups = []
     i = 0
     while i < len(row):
@@ -20,11 +23,10 @@ def analyze_row(img, row_num):
             j += 1
             if j == len(row):
                 break
-        groups.append(current_group)
+        if not (np.array_equal(current_color, background)):
+            groups.append(current_group)
         i = j
-    print(groups)
-
+    return groups
 
 img = cv2.imread('pics/test1.png')
 background = np.array([255, 255, 255])
-analyze_row(img, 14)
